c7409996317b6760870ccffeaecc72168ed3a733
Christel Mogollon <christel.floresm@gmail.com>
2018-12-19 10:35:36 -0500

    added form to user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  resources :users
+  get '/about', to: 'pages#about'
+  get '/faq', to: 'pages#faq'
+
 end
